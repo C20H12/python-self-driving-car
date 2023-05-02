419c5399c427c764f70378bb8dfe961d1f73460f
C20H12 <marvinm2@educbe.ca>
2023-05-02 10:27:39 -0600

    intersectopr

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import pygame as pg
+
+
 def lerp(A: float, B: float, t: float):
   '''
   A - first number
@@ -7,5 +10,34 @@ def lerp(A: float, B: float, t: float):
   '''
   return A + (B - A) * t
 
-def get_interersection(line_1, line_2):
-  pass
+def get_interersection_point(
+  line_1_start: pg.Vector2, 
+  line_1_end: pg.Vector2, 
+  line_2_start: pg.Vector2, 
+  line_2_end: pg.Vector2
+):
+  slope_1 = (line_1_start.y - line_1_end.y) / (line_1_start.x - line_1_end.x)
+  slope_2 = (line_2_start.y - line_2_end.y) / (line_2_start.x - line_2_end.x)
+  
+  slope_diff = slope_1 - slope_2
+  if slope_diff == 0:
+    return None
+  
+  intercept_x = (line_2_start.y - slope_2 * line_2_start.x \
+                 - line_1_start.y + slope_1 * line_1_start.x) \
+                 / (slope_1 - slope_2)
+  intercept_y = slope_1 * intercept_x + line_1_start.y - slope_1 * line_1_start.x
+  
+  line_1_x_bounds = (min(line_1_start.x, line_1_end.x), max(line_1_start.x, line_1_end.x))
+  line_1_y_bounds = (min(line_1_start.y, line_1_end.y), max(line_1_start.y, line_1_end.y))
+  line_2_x_bounds = (min(line_2_start.x, line_2_end.x), max(line_2_start.x, line_2_end.x))
+  line_2_y_bounds = (min(line_2_start.y, line_2_end.y), max(line_2_start.y, line_2_end.y))
+
+  if intercept_x < line_1_x_bounds[0] or intercept_x > line_1_x_bounds[1] \
+    or intercept_x < line_2_x_bounds[0] or intercept_x > line_2_x_bounds[1] \
+    or intercept_y < line_1_y_bounds[0] or intercept_y > line_1_y_bounds[1] \
+    or intercept_y < line_2_y_bounds[0] or intercept_y > line_2_y_bounds[1]:
+    return None
+  
+  return pg.Vector2(intercept_x, intercept_y)
+
